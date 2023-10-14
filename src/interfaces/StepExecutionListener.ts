cc7981ce62e5975904aedcf3f00a30588978242d
Hyun Oh <hyunnoh01@gmail.com>
2023-10-14 18:51:36 +0900

    feat: add StepListener for common parent interface

--- src/interfaces/StepExecutionListener.ts
+++ src/interfaces/StepExecutionListener.ts
@@ -1,7 +1,8 @@
 import { ExitStatus } from "./ExitStatus";
 import { StepExecution } from "./StepExecution";
+import { StepListener } from "./StepListener";
 
-export interface StepExecutionListener {
+export interface StepExecutionListener extends StepListener {
   afterStep(stepExecution: StepExecution): ExitStatus;
   beforeStep(stepExecution: StepExecution): void;
 }
