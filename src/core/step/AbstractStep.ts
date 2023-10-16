1e5ddf32bab06a8a1d0bf8fba86fbaf22d3df9fb
Hyun Oh <hyunnoh01@gmail.com>
2023-10-17 00:46:52 +0900

    feat: implement AbstractStep.execute()

--- src/core/step/AbstractStep.ts
+++ src/core/step/AbstractStep.ts
@@ -1,10 +1,16 @@
+import { ExecutionContext } from "../../item/ExecutionContext";
 import { Step } from "../Step";
 import { StepExecution } from "../StepExecution";
+import { StepExecutionListener } from "../StepExecutionListener";
+import { JobRepository } from "../repository/JobRepository";
 
 export abstract class AbstractStep implements Step {
   isAllowStartingIfComplete: boolean;
+  jobRepository: JobRepository;
   name: string;
   startLimit: number;
+
+  private listeners: StepExecutionListener[] = [];
 
   constructor();
   constructor(name: string);
@@ -12,5 +18,33 @@ export abstract class AbstractStep implements Step {
     this.name = name || "";
   }
 
-  async execute(stepExecution: StepExecution): Promise<void> {}
+  protected abstract doExecute(stepExecution: StepExecution): Promise<void>;
+
+  async execute(stepExecution: StepExecution): Promise<void> {
+    this.listeners.forEach((listener) => {
+      listener.beforeStep(stepExecution);
+    });
+    await this.doExecute(stepExecution);
+    this.listeners.forEach((listener) => {
+      listener.afterStep(stepExecution);
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  protected open(_ctx: ExecutionContext) {
+    // Does nothing by default.
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  protected close(_ctx: ExecutionContext) {
+    // Does nothing by default.
+  }
+
+  registerStepExecutionListener(listener: StepExecutionListener) {
+    this.listeners.push(listener);
+  }
+
+  setStepExecutionListeners(listeners: StepExecutionListener[]) {
+    this.listeners = listeners;
+  }
 }
