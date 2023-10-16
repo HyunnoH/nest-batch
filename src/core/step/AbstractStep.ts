0dbacec8a95a81a0f49e7bedf73b7c1bbde27209
Hyun Oh <hyunnoh01@gmail.com>
2023-10-17 00:42:02 +0900

    refactor: remove getter and setter from AbstractStep

--- src/core/step/AbstractStep.ts
+++ src/core/step/AbstractStep.ts
@@ -3,28 +3,13 @@ import { StepExecution } from "../StepExecution";
 
 export abstract class AbstractStep implements Step {
   isAllowStartingIfComplete: boolean;
-
-  private _name: string;
-  private _startLimit: number;
-
-  set name(name: string) {
-    this._name = name;
-  }
-  get name() {
-    return this._name;
-  }
-
-  set startLimit(startLimit: number) {
-    this._startLimit = startLimit;
-  }
-  get startLimit() {
-    return this._startLimit;
-  }
+  name: string;
+  startLimit: number;
 
   constructor();
   constructor(name: string);
   constructor(name?: string) {
-    this._name = name || "";
+    this.name = name || "";
   }
 
   async execute(stepExecution: StepExecution): Promise<void> {}
